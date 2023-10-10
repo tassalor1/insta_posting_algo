ca2772f5f3d6b3a4359a832d78a3ea9f0b632ab8
tassalor1 <walkerconnor14@gmail.com>
2023-10-10 22:41:44 +0100

    script to post using selenium

--- scripts/insta_bot.py
+++ scripts/insta_bot.py
@@ -4,6 +4,7 @@ from credentials import insta_user, insta_password
 from time import sleep
 
 class Bot():
+
     def __init__(self):
         self.login(insta_user, insta_password)
         self.post()
@@ -35,11 +36,31 @@ class Bot():
         login_button = self.driver.find_element(By.CSS_SELECTOR, "button[type='submit']")
         login_button.click()
 
-        self.driver.get("https://www.instagram.com/clave.xt/")
+        # self.driver.get("https://www.instagram.com/clave.xt/")
         sleep(5)
 
     def post(self):
+        try:
+            sav_info = self.driver.find_element(By.CSS_SELECTOR, "div[role='button'].btn-class")
+            sav_info.click()
+        except Exception as e:
+            print(f"Could not find the save info button: {e}")
 
+        try:
+            create_button = self.driver.find_element(By.XPATH,'// *[ @ id = "mount_0_0_e0"] / div / div / div[2] '
+                                                              '/ div / div / div / div[1] / div[1] / div[1] / div / div '
+                                                              '/ div / div / div[2] / div[7] / div / span / div / a / div '
+                                                              '/ div[2] / div / div / span / span')
+            create_button.click()
+        except Exception as e:
+            print(f"Could not find the create button: {e}")
+
+        try:
+            sel_comp = self.driver.find_element(By.XPATH, '/ html / body / div[8] / div[1] / div / div[3] / div / div / div '
+                                           '/ div / div / div / div / div[2] / div[1] / div / div / div[2] / div / button')
+            sel_comp.click()
+        except Exception as e:
+            print(f"Could not find the select button: {e}")
 
 def main():
     my_bot = Bot()
@@ -47,3 +68,4 @@ def main():
 
 if __name__ == '__main__':
     main()
+
